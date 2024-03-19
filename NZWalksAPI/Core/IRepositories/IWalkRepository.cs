0c7708fb68361d318db5a72a2f9e38b845ce967a
Phuong <lephuong10142002@gmail.com>
2024-03-19 14:43:52 +0700

    Filter, sort, pagination Walk

--- NZWalksAPI/Core/IRepositories/IWalkRepository.cs
+++ NZWalksAPI/Core/IRepositories/IWalkRepository.cs
@@ -6,7 +6,8 @@ namespace NZWalksAPI.Core.IRepositories
     {
         Task<Walk> CreateAsync(Walk walk);
 
-        Task<List<Walk>> GetAllAsync();
+        Task<List<Walk>> GetAllAsync(string? filerOn = null, string? filterQuery = null, string? sortBy = null
+            , bool isAscending = true, int pageNumber = 1, int pageSize = 1000);
 
         Task<Walk?> GetByIdAsync(Guid id);
 
